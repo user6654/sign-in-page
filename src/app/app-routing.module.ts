2a3d13b8c635ba65fbadfe9013ea884ce6433534
pmerencillo <pmerencillo+github-user6654@gmail.com>
2018-11-15 20:23:06 +0800

    hello

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SigninComponent } from './signin/signin.component';
+
+const routes: Routes = [
+  {path: 'signin', component:SigninComponent},
+  // {path: '', redirectTo: 'signin', pathMatch: 'full'}, //for "/" only
+  {path: '**', redirectTo: 'signin', pathMatch: 'full'}   //for "/<any>"
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,12 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, FormBuilder } from "@angular/forms";
+
+import { userModel } from "../model/user";
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  selector: "app-signin",
+  templateUrl: "./signin.component.html",
+  styleUrls: ["./signin.component.scss"]
 })
-export class SigninComponent {
-  variableUserId    = new FormControl('');
-  variablePassword  = new FormControl('');
+
+export class SigninComponent implements OnInit {
+  public users: userModel[];
+  public signInForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.users = [
+      { username: 'nemo', password: 'qwer1234' },
+      { username: 'nema', password: 'qwer1234' }
+    ];
+  }
+
+  ngOnInit() {
+    this.initForm()
+  }
+
+  private initForm() {
+    this.signInForm = this.fb.group({
+
+    username : new FormControl(''),
+    password : new FormControl('', {updateOn: 'blur'})
+    })
+  }
+
+
+  public onSubmit() {
+    const user = this.signInForm.value;
+    this.matchUser(user);
+  }
+
+  public matchUser(user: userModel) {
+    this.users.some(compareToValidSignIns => {
+      if(compareToValidSignIns.username === user.username &&
+         compareToValidSignIns.password === user.password){
+           console.log('Login Accepted')
+           return true;
+         } else {
+           console.log('Invalid Login Input')
+         }
+    })
+  }
+///*
+  get username() {
+    return this.signInForm.get('username').value
+  }
+
+  get password() {
+    return this.signInForm.get('password').value
+  }
+//*/
 }
